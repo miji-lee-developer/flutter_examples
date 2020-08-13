4b115d5d525b7f6a2f454994397922d41d587f2e
miji <miji.lee.developer@gmail.com>
2020-08-13 20:45:53 +0900

    Day 2. example - expanded Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,49 +11,120 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Basic Row Widget'),
+      home: ExpandedWidget(title: 'ExpandedWidget'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+ExpandedWidgetState pageState;
+
+class ExpandedWidget extends StatefulWidget {
+  ExpandedWidget({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  ExpandedWidgetState createState(){
+    pageState = ExpandedWidgetState();
+    return pageState;
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class ExpandedWidgetState extends State<ExpandedWidget>{
+  List<int> flexList = [1, 1, 1, 1, 1, 1];
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Row(
+      appBar: AppBar(title: Text(widget.title)),
+      body: ListView(
         children: <Widget>[
-          boxWidget(),
-          boxWidget(),
-          boxWidget(),
+          Text("Row"),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.indigo, width: 0.5)
+            ),
+            height: 100,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: flexList[0],
+                  child: boxWidget(0),
+                ),
+                Expanded(
+                  flex: flexList[1],
+                  child: boxWidget(1),
+                ),
+                Expanded(
+                  flex: flexList[2],
+                  child: boxWidget(2),
+                ),
+              ],
+            ),
+          ),
+          Text("\nColumn"),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.indigo, width: 0.5)
+            ),
+            height: 300,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: flexList[3],
+                  child: boxWidget(3),
+                ),
+                Expanded(
+                  flex: flexList[4],
+                  child: boxWidget(4),
+                ),
+                Expanded(
+                  flex: flexList[5],
+                  child: boxWidget(5),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: FlatButton(
+              color: Colors.orange,
+              child: Text("reset", style: TextStyle(color: Colors.white)),
+              onPressed: (){
+                setState((){
+                  flexList = [1, 1, 1, 1, 1, 1];
+                });
+              },
+            ),
+          ),
         ],
       ),
     );
   }
 
-  boxWidget(){
-    return Container(
-      height: 50,
-      width: 50,
-      decoration: BoxDecoration(
-        color: Colors.blue[700],
-        border: Border.all(color: Colors.indigo, width: 0.5)
+  boxWidget(int flexIndex){
+    return GestureDetector(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.blue[400],
+            border: Border.all(color: Colors.indigo, width: 0.5)
+        ),
+        child: Center(
+          child: Text("flex: ${flexList[flexIndex]}",
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+              )),
+        ),
       ),
-      child: Center(
-        child: Text("Box", style: TextStyle(fontSize: 12, color: Colors.white))
-      ),
+      onTap: (){
+        print(flexIndex);
+        setState((){
+          flexList[flexIndex] = flexList[flexIndex] + 1;
+        });
+      },
     );
   }
 }
