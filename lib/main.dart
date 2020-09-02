7398d84f2275bcb470f96ff9e1e774ea7aae77f9
miji <miji.lee.developer@gmail.com>
2020-09-02 19:33:19 +0900

    Day 21. example - Handle Image | Load Cached Image | cached_network_image plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,34 +12,31 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoadFadeInImageFromNetwork(title: 'Load Image From Network'),
+      home: LoadCachedNetworkImage(title: 'Load Image From Network'),
     );
   }
 }
 
-LoadFadeInImageFromNetworkState pageState;
+LoadCachedNetworkImageState pageState;
 
-class LoadFadeInImageFromNetwork extends StatefulWidget {
-  LoadFadeInImageFromNetwork({Key key, this.title}) : super(key: key);
+class LoadCachedNetworkImage extends StatefulWidget {
+  LoadCachedNetworkImage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  LoadFadeInImageFromNetworkState createState() {
-    pageState = LoadFadeInImageFromNetworkState();
+  LoadCachedNetworkImageState createState() {
+    pageState = LoadCachedNetworkImageState();
     return pageState;
   }
 }
 
-class LoadFadeInImageFromNetworkState extends State<LoadFadeInImageFromNetwork> {
-  String url = "https://picsum.photos/300?${Random().nextInt(100)}";
-
-  final scaffoldKey = GlobalKey<ScaffoldState>();
+class LoadCachedNetworkImageState extends State<LoadCachedNetworkImage> {
+  String url = "https://picsum.photos/300";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
       appBar: AppBar(title: Text(widget.title)),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -48,19 +44,12 @@ class LoadFadeInImageFromNetworkState extends State<LoadFadeInImageFromNetwork> 
         children: <Widget>[
           Container(
             height: 300,
-            child: Stack(
-              alignment: Alignment(0, 0),
-              children: <Widget>[
-                Center(
-                  child: CircularProgressIndicator(),
-                ),
-                Center(
-                  child: FadeInImage.memoryNetwork(
-                    placeholder: kTransparentImage,
-                    image: url,
-                  ),
-                ),
-              ],
+            child: Center(
+              child: CachedNetworkImage(
+                imageUrl: url,
+                placeholder: (context, url) => new CircularProgressIndicator(),
+                errorWidget: (context, url, error) => new Icon(Icons.error),
+              ),
             ),
           ),
           Text(url),
@@ -69,34 +58,12 @@ class LoadFadeInImageFromNetworkState extends State<LoadFadeInImageFromNetwork> 
             margin: const EdgeInsets.all(15),
             padding: const EdgeInsets.all(15),
             child: Text(
-              "* Loading a network image takes time to download the image data.",
+              "* Cached images are loaded quickly without re-downloading.",
               style: TextStyle(color: Colors.white),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(5),
-            padding: const EdgeInsets.only(top: 3, right: 15),
-            alignment: Alignment.bottomRight,
-            child: Container(
-              padding: const EdgeInsets.all(0),
-              height: 40,
-              width: 40,
-              child: FloatingActionButton(
-                child: Icon(Icons.refresh),
-                onPressed: () {
-                  setState(() {
-                    getImage();
-                  });
-                },
-              ),
             ),
           ),
         ],
       ),
     );
-  }
-
-  getImage() {
-    url = "https://picsum.photos/300?${Random().nextInt(100)}";
   }
 }
