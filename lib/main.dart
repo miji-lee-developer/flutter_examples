768dba344554a56e8a3cb9910c46894a23f4aaf0
miji <miji.lee.developer@gmail.com>
2020-09-03 19:29:49 +0900

    Day 22. example - Handle Image | Fit Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,54 +11,185 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoadCachedNetworkImage(title: 'Load Image From Network'),
+      home: FitImageDemo(title: 'Fit Image Demo'),
     );
   }
 }
 
-LoadCachedNetworkImageState pageState;
+FitImageDemoState pageState;
 
-class LoadCachedNetworkImage extends StatefulWidget {
-  LoadCachedNetworkImage({Key key, this.title}) : super(key: key);
+class FitImageDemo extends StatefulWidget {
+  FitImageDemo({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  LoadCachedNetworkImageState createState() {
-    pageState = LoadCachedNetworkImageState();
+  FitImageDemoState createState() {
+    pageState = FitImageDemoState();
     return pageState;
   }
 }
 
-class LoadCachedNetworkImageState extends State<LoadCachedNetworkImage> {
-  String url = "https://picsum.photos/300";
+class FitImageDemoState extends State<FitImageDemo> {
+  String imagePath = "assets/images/300by300.jpg";
+  double boxHeight = 300;
+  double boxWidth = 300;
+  BoxFit fit = BoxFit.fill;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(
         children: <Widget>[
-          Container(
-            height: 300,
-            child: Center(
-              child: CachedNetworkImage(
-                imageUrl: url,
-                placeholder: (context, url) => new CircularProgressIndicator(),
-                errorWidget: (context, url, error) => new Icon(Icons.error),
+          Padding(padding: const EdgeInsets.only(top: 25)),
+          Center(
+            child: Container(
+              height: 300,
+              width: 300,
+              color: Colors.grey[300],
+              alignment: Alignment(0, 0),
+              child: Image.asset(
+                imagePath,
+                fit: fit,
+                width: boxWidth,
+                height: boxHeight,
               ),
             ),
           ),
-          Text(url),
-          Container(
-            decoration: BoxDecoration(color: Colors.orangeAccent),
-            margin: const EdgeInsets.all(15),
-            padding: const EdgeInsets.all(15),
-            child: Text(
-              "* Cached images are loaded quickly without re-downloading.",
-              style: TextStyle(color: Colors.white),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Image:"),
+                RaisedButton(
+                  child: Text("300 X 300"),
+                  onPressed: () {
+                    setState(() {
+                      imagePath = "assets/images/300by300.jpg";
+                      boxHeight = 300;
+                      boxWidth = 300;
+                    });
+                  },
+                ),
+                RaisedButton(
+                  child: Text("200 X 300"),
+                  onPressed: () {
+                    setState(() {
+                      imagePath = "assets/images/200by300.jpg";
+                      boxHeight = 300;
+                      boxWidth = 200;
+                    });
+                  },
+                ),
+                RaisedButton(
+                  child: Text("300 X 200"),
+                  onPressed: () {
+                    setState(() {
+                      imagePath = "assets/images/300by200.jpg";
+                      boxHeight = 200;
+                      boxWidth = 300;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Box Width:"),
+                Slider(
+                  value: boxWidth,
+                  min: 50,
+                  max: 300,
+                  divisions: 25,
+                  onChanged: (value) {
+                    setState(() {
+                      boxWidth = value;
+                    });
+                  },
+                ),
+                Text(boxWidth.toInt().toString())
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Box Height:"),
+                Slider(
+                  value: boxHeight,
+                  min: 50,
+                  max: 300,
+                  divisions: 25,
+                  onChanged: (value) {
+                    setState(() {
+                      boxHeight = value;
+                    });
+                  },
+                ),
+                Text(boxHeight.toInt().toString())
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Box Fit:"),
+                Expanded(child: Container()),
+                PopupMenuButton<BoxFit>(
+                  onSelected: (BoxFit result) {
+                    print(result);
+                    setState(() {
+                      fit = result;
+                    });
+                  },
+                  child: Container(
+                    width: 150,
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+                    child: Text(fit.toString()),
+                  ),
+                  itemBuilder: (BuildContext context) => <PopupMenuEntry<BoxFit>>[
+                    const PopupMenuItem<BoxFit>(
+                      value: BoxFit.contain,
+                      child: Text("BoxFit.contain"),
+                    ),
+                    const PopupMenuItem<BoxFit>(
+                      value: BoxFit.cover,
+                      child: Text("BoxFit.cover"),
+                    ),
+                    const PopupMenuItem<BoxFit>(
+                      value: BoxFit.fill,
+                      child: Text("BoxFit.fill"),
+                    ),
+                    const PopupMenuItem<BoxFit>(
+                      value: BoxFit.fitHeight,
+                      child: Text("BoxFit.fitHeight"),
+                    ),
+                    const PopupMenuItem<BoxFit>(
+                      value: BoxFit.fitWidth,
+                      child: Text("BoxFit.fitWidth"),
+                    ),
+                    const PopupMenuItem<BoxFit>(
+                      value: BoxFit.none,
+                      child: Text("BoxFit.none"),
+                    ),
+                    const PopupMenuItem<BoxFit>(
+                      value: BoxFit.scaleDown,
+                      child: Text("BoxFit.scaleDown"),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
