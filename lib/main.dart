8af29f88bde23e8941922c9b1210e37737a08efc
miji <miji.lee.developer@gmail.com>
2020-09-04 23:03:30 +0900

    Day 23. example - Handle Image | Load Image from Gallery | image_picker plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,189 +13,74 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: FitImageDemo(title: 'Fit Image Demo'),
+      home: LoadImageFromGallery(title: 'Load Image From Gallery'),
     );
   }
 }
 
-FitImageDemoState pageState;
+LoadImageFromGalleryState pageState;
 
-class FitImageDemo extends StatefulWidget {
-  FitImageDemo({Key key, this.title}) : super(key: key);
+class LoadImageFromGallery extends StatefulWidget {
+  LoadImageFromGallery({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  FitImageDemoState createState() {
-    pageState = FitImageDemoState();
+  LoadImageFromGalleryState createState() {
+    pageState = LoadImageFromGalleryState();
     return pageState;
   }
 }
 
-class FitImageDemoState extends State<FitImageDemo> {
-  String imagePath = "assets/images/300by300.jpg";
-  double boxHeight = 300;
-  double boxWidth = 300;
-  BoxFit fit = BoxFit.fill;
+class LoadImageFromGalleryState extends State<LoadImageFromGallery> {
+  File _image;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: ListView(
-        children: <Widget>[
-          Padding(padding: const EdgeInsets.only(top: 25)),
-          Center(
-            child: Container(
-              height: 300,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(10),
               width: 300,
-              color: Colors.grey[300],
-              alignment: Alignment(0, 0),
-              child: Image.asset(
-                imagePath,
-                fit: fit,
-                width: boxWidth,
-                height: boxHeight,
-              ),
+              height: 300,
+              child: (_image != null) ? Image.file(_image) : Placeholder(),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text("Image:"),
                 RaisedButton(
-                  child: Text("300 X 300"),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Gallery"),
                   onPressed: () {
-                    setState(() {
-                      imagePath = "assets/images/300by300.jpg";
-                      boxHeight = 300;
-                      boxWidth = 300;
-                    });
+                    getImage(ImageSource.gallery);
                   },
                 ),
                 RaisedButton(
-                  child: Text("200 X 300"),
+                  color: Colors.orange,
+                  textColor: Colors.white,
+                  child: Text("Camera"),
                   onPressed: () {
-                    setState(() {
-                      imagePath = "assets/images/200by300.jpg";
-                      boxHeight = 300;
-                      boxWidth = 200;
-                    });
-                  },
-                ),
-                RaisedButton(
-                  child: Text("300 X 200"),
-                  onPressed: () {
-                    setState(() {
-                      imagePath = "assets/images/300by200.jpg";
-                      boxHeight = 200;
-                      boxWidth = 300;
-                    });
+                    getImage(ImageSource.camera);
                   },
                 ),
               ],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text("Box Width:"),
-                Slider(
-                  value: boxWidth,
-                  min: 50,
-                  max: 300,
-                  divisions: 25,
-                  onChanged: (value) {
-                    setState(() {
-                      boxWidth = value;
-                    });
-                  },
-                ),
-                Text(boxWidth.toInt().toString())
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text("Box Height:"),
-                Slider(
-                  value: boxHeight,
-                  min: 50,
-                  max: 300,
-                  divisions: 25,
-                  onChanged: (value) {
-                    setState(() {
-                      boxHeight = value;
-                    });
-                  },
-                ),
-                Text(boxHeight.toInt().toString())
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text("Box Fit:"),
-                Expanded(child: Container()),
-                PopupMenuButton<BoxFit>(
-                  onSelected: (BoxFit result) {
-                    print(result);
-                    setState(() {
-                      fit = result;
-                    });
-                  },
-                  child: Container(
-                    width: 150,
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
-                    child: Text(fit.toString()),
-                  ),
-                  itemBuilder: (BuildContext context) => <PopupMenuEntry<BoxFit>>[
-                    const PopupMenuItem<BoxFit>(
-                      value: BoxFit.contain,
-                      child: Text("BoxFit.contain"),
-                    ),
-                    const PopupMenuItem<BoxFit>(
-                      value: BoxFit.cover,
-                      child: Text("BoxFit.cover"),
-                    ),
-                    const PopupMenuItem<BoxFit>(
-                      value: BoxFit.fill,
-                      child: Text("BoxFit.fill"),
-                    ),
-                    const PopupMenuItem<BoxFit>(
-                      value: BoxFit.fitHeight,
-                      child: Text("BoxFit.fitHeight"),
-                    ),
-                    const PopupMenuItem<BoxFit>(
-                      value: BoxFit.fitWidth,
-                      child: Text("BoxFit.fitWidth"),
-                    ),
-                    const PopupMenuItem<BoxFit>(
-                      value: BoxFit.none,
-                      child: Text("BoxFit.none"),
-                    ),
-                    const PopupMenuItem<BoxFit>(
-                      value: BoxFit.scaleDown,
-                      child: Text("BoxFit.scaleDown"),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  void getImage(ImageSource source) async {
+    print("getImageFromGallery");
+    var image = await ImagePicker.pickImage(source: source);
+
+    setState(() {
+      _image = image;
+    });
   }
 }
