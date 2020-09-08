f87875a2968e98d7302c0c9a9b7ec8fa1d74370a
miji <miji.lee.developer@gmail.com>
2020-09-08 19:46:44 +0900

    Day 27. example - How to get app information | App Name, App ID, Version, BuildNumber | package_info plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:package_info/package_info.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,218 +12,79 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ParseJsonDemo(title: 'Parse Json Data'),
+      home: AppInfoDemo(title: 'App Information'),
     );
   }
 }
 
-ParseJsonDemoState pageState;
+AppInfoDemoState pageState;
 
-class ParseJsonDemo extends StatefulWidget {
-  ParseJsonDemo({Key key, this.title}) : super(key: key);
+class AppInfoDemo extends StatefulWidget {
+  AppInfoDemo({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  ParseJsonDemoState createState() {
-    pageState = ParseJsonDemoState();
+  AppInfoDemoState createState() {
+    pageState = AppInfoDemoState();
     return pageState;
   }
 }
 
-class ParseJsonDemoState extends State<ParseJsonDemo> {
-  String filePath = "assets/files/albums.json"; // Json File
-  String fileText = ""; // String to store Json Data
-  List<Album> albums = List<Album>(); // List to store Parsed Json Data
+class AppInfoDemoState extends State<AppInfoDemo> {
+  String appName = "";
+  String appID = "";
+  String version = "";
+  String buildNumber = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getAppInfo();
+  }
+
+  void getAppInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      appName = packageInfo.appName;
+      appID = packageInfo.packageName;
+      version = packageInfo.version;
+      buildNumber = packageInfo.buildNumber;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Row(
-              children: <Widget>[
-                Text("File Info: "),
-                Container(width: 10),
-                Expanded(child: customTextContainer(filePath)),
-                Container(width: 10),
-                customButton("Read", () { readFile(); }),
-              ],
+          Card(
+            child: ListTile(
+              title: Text("App Name"),
+              subtitle: Text(appName),
             ),
           ),
-          Container(
-            height: 200,
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              border: Border.all(color: Colors.grey, width: 1),
-            ),
-            child: Center(
-              child: SingleChildScrollView(
-                child: Text(
-                  fileText,
-                  style: TextStyle(fontSize: 13),
-                ),
-              ),
+          Card(
+            child: ListTile(
+              title: Text("Package Name (AppID)"),
+              subtitle: Text(appID),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text("Parse Json File String to Albums"),
-                customButton(
-                  "parse",
-                  (fileText.length > 0)
-                  ? () { parseJson(); }
-                  : null
-                ),
-              ],
+          Card(
+            child: ListTile(
+              title: Text("Version"),
+              subtitle: Text(version),
             ),
           ),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10),
-              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                border: Border.all(color: Colors.grey, width: 1),
-              ),
-              child: ListView.builder(
-                itemCount: albums.length,
-                itemBuilder: (Context, index) {
-                  Album album = albums[index];
-                  return Card(
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            height: 30,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                customBoxContainer(Text("userID: "), left: true, top: true, right: true),
-                                customBoxContainer(Text(album.userId.toString()), top: true, right: true),
-                                customBoxContainer(Text("id: "), top: true, right: true),
-                                customBoxContainer(Text(album.id.toString()), top: true, right: true),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            height: 50,
-                            child: Row(
-                              children: <Widget>[
-                                customBoxContainer(Text("title: "), left: true, top: true, bottom: true),
-                                customBoxContainer(Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    album.title,
-                                    style: TextStyle(fontSize: 13),
-                                  ),
-                                ),
-                                left: true, top: true, right: true, bottom: true, flex: 4)
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                }
-              ),
+          Card(
+            child: ListTile(
+              title: Text("Build Number"),
+              subtitle: Text(buildNumber),
             ),
           ),
         ],
       ),
-    );
-  }
-
-  readFile() async {
-    String text = await rootBundle.loadString(filePath);
-    setState(() {
-      fileText = text;
-    });
-  }
-
-  parseJson() {
-    final parsed = json.decode(fileText).cast<Map<String, dynamic>>();
-    setState(() {
-      albums = parsed.map<Album>((json) => Album.fromJson(json)).toList();
-    });
-  }
-
-  customBoxContainer(Widget child,
-      {bool left = false,
-        bool top = false,
-        bool right = false,
-        bool bottom = false,
-        int flex = 1}) {
-    BorderSide borderSide = BorderSide(color: Colors.grey[500], width: 1);
-    return Expanded(
-      flex: flex,
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            left: (left == true) ? borderSide : BorderSide.none,
-            top: (top == true) ? borderSide : BorderSide.none,
-            right: (right == true) ? borderSide : BorderSide.none,
-            bottom: (bottom == true) ? borderSide : BorderSide.none,
-          ),
-        ),
-        alignment: Alignment(0, 0),
-        child: child,
-      ),
-    );
-  }
-
-  customTextContainer(String text) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey, width: 1),
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Text(text),
-    );
-  }
-
-  customButton(String text, Null Function() onPressed) {
-    return SizedBox(
-      height: 30,
-      width: 60,
-      child: FlatButton(
-        padding: const EdgeInsets.all(0),
-        color: Colors.blue,
-        disabledColor: Colors.grey,
-        textColor: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 13),
-        ),
-        onPressed: onPressed,
-      ),
-    );
-  }
-}
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  Album({this.userId, this.id, this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'] as int,
-      id: json['id'] as int,
-      title: json['title'] as String
     );
   }
 }
