459abcaca0a7ba18c1d14bcd9bd9f24948832190
miji <miji.lee.developer@gmail.com>
2020-09-14 19:49:51 +0900

    Day 33. example - Http Request | Get method | http plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -12,31 +13,32 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: OpenWebBrowser(title: 'Open Web Browser'),
+      home: HttpGetDemo(title: 'Http Get Request'),
     );
   }
 }
 
-OpenWebBrowserState pageState;
+HttpGetDemoState pageState;
 
-class OpenWebBrowser extends StatefulWidget {
-  OpenWebBrowser({Key key, this.title}) : super(key: key);
+class HttpGetDemo extends StatefulWidget {
+  HttpGetDemo({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  OpenWebBrowserState createState() {
-    pageState = OpenWebBrowserState();
+  HttpGetDemoState createState() {
+    pageState = HttpGetDemoState();
     return pageState;
   }
 }
 
-class OpenWebBrowserState extends State<OpenWebBrowser> {
+class HttpGetDemoState extends State<HttpGetDemo> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
-  TextEditingController teCon = TextEditingController(text: "http://google.com");
-
+  TextEditingController teCon = TextEditingController(text: "https://jsonplaceholder.typicode.com/albums");
   FocusNode myFocusNode = FocusNode();
+  String response = "";
+  TextStyle ts = TextStyle(fontSize: 12);
 
   @override
   void dispose() {
@@ -52,22 +54,28 @@ class OpenWebBrowserState extends State<OpenWebBrowser> {
       appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            customButton("Google", "https://google.com"),
-            customButton("Flutter", "https://flutter.dev"),
-            customButton("Facebook", "https://facebook.com"),
-            Divider(color: Colors.grey, height: 50, indent: 50, endIndent: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                customButton("albums", "https://jsonplaceholder.typicode.com/albums"),
+                customButton("comments", "https://jsonplaceholder.typicode.com/comments"),
+                customButton("Google", "https://google.com"),
+                customButton("Flutter", "https://flutter.dev"),
+                customButton("Facebook", "https://facebook.com"),
+              ],
+            ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Text("URL: "),
+                  Text("URL: ", style: ts),
                   Expanded(
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
                       child: TextField(
+                        style: TextStyle(fontSize: 10),
                         decoration: InputDecoration(),
                         controller: teCon,
                         focusNode: myFocusNode,
@@ -75,29 +83,45 @@ class OpenWebBrowserState extends State<OpenWebBrowser> {
                     ),
                   ),
                   Container(
-                    width: 60,
+                    width: 50,
                     child: RaisedButton(
                       padding: const EdgeInsets.all(0),
                       color: Colors.blueGrey,
                       textColor: Colors.white,
-                      child: Text("launch"),
+                      child: Text("GET", style: ts),
                       onPressed: () {
-                        _launchUrl(teCon.text.toString());
+                        _getUrl(teCon.text.toString());
                       },
                     ),
                   ),
                   Container(
                     padding: const EdgeInsets.only(left: 5),
-                    width: 65,
+                    width: 55,
                     child: RaisedButton(
                       padding: const EdgeInsets.all(0),
                       color: Colors.grey,
                       textColor: Colors.white,
-                      child: Text("clear"),
+                      child: Text("launch", style: ts),
                       onPressed: () {
-                        FocusScope.of(context).requestFocus(myFocusNode);
-                        teCon.text = "http://";
+                        _launchUrl(teCon.text.toString());
                       },
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.grey, width: 1),
+                      ),
+                      child: Center(
+                        child: (response == null)
+                            ? CircularProgressIndicator()
+                            : SingleChildScrollView(
+                              child: Text(response, style: TextStyle(fontSize: 13))
+                        ),
+                      ),
                     ),
                   ),
                 ],
@@ -111,16 +135,27 @@ class OpenWebBrowserState extends State<OpenWebBrowser> {
 
   customButton(String text, String url) {
     return Container(
-      width: 250,
+      width: 70,
       child: RaisedButton(
+        padding: const EdgeInsets.all(0),
         color: Colors.blue,
         textColor: Colors.white,
-        child: Text(text),
+        child: Text(text, style: ts),
         onPressed: () {
           teCon.text = url;
         },
       ),
     );
+  }
+
+  _getUrl(String url) async {
+    setState(() {
+      response = null;
+    });
+    var temp = await http.get(url);
+    setState(() {
+      response = temp.body;
+    });
   }
 
   _launchUrl(String url) async {
