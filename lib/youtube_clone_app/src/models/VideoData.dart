5c20beafb360c614a4da8d384a07ff34e274989a
miji <miji.lee.developer@gmail.com>
2020-09-27 20:45:26 +0900

    Day 46. example - Youtube clone app

---
+++ lib/youtube_clone_app/src/models/VideoData.dart
@@ -0,0 +1,34 @@
+import 'ChannelData.dart';
+
+class VideoData {
+  String ownerChannelId;
+  String thumbnailUrl;
+  String title;
+  String viewCount;
+  String publishDate;
+  ChannelData channelData;
+
+  VideoData(Map videoJsonData) {
+    this.ownerChannelId = videoJsonData["snippet"]["channelId"];
+    this.thumbnailUrl = videoJsonData["snippet"]["thumbnails"]["high"]["url"];
+    this.title = videoJsonData["snippet"]["title"];
+    this.viewCount = videoJsonData["statistics"]["viewCount"];
+    this.publishDate = videoJsonData["snippet"]["publishedAt"];
+  }
+
+  VideoData.includeChannelData(Map videoJsonData, Map channelJsonData) {
+    VideoData(videoJsonData);
+    this.channelData = ChannelData(channelJsonData);
+  }
+
+  set channelDataFromJson(Map channelJsonData) {
+    this.channelData = new ChannelData(channelJsonData);
+  }
+
+  get getOwnerChannelId => this.ownerChannelId;
+  get getThumbnailUrl => this.thumbnailUrl;
+  get getTitle => this.title;
+  get getViewCount => this.viewCount;
+  get getPublishedDate => this.publishDate;
+  get getChannelData => this.channelData;
+}
